f194a29cead674edf94efe653ce10792d78c51eb
Leo Jiang <leo.jiang.dev@gmail.com>
2012-05-18 00:19:49 +0800

    Migrate the files from code.google.com/p/programmer-magicbox

---
+++ git/gitconfig.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git config --global user.name "Leo Jiang"
+git config --global user.email leo.jiang.dev@gmail.com
+git config --global core.editor vim
+git config --global core.excludesfile "~/.gitignore_global"
+git config --global core.autocrlf input
