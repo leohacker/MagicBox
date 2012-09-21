48b35c7bfba8d45ad43e8abde590e5738d28ff8c
Leo Jiang <leo.jiang.dev@gmail.com>
2012-09-21 12:43:55 +0800

    add aliases in git config shell script.

--- git/gitconfig.sh
+++ git/gitconfig.sh
@@ -4,4 +4,6 @@ git config --global user.email leo.jiang.dev@gmail.com
 git config --global core.editor vim
 git config --global core.excludesfile "~/.gitignore_global"
 git config --global core.autocrlf input
+git config --global alias.cm commit
+git config --global alias.st status
 cp _gitignore_global ~/.gitignore_global
