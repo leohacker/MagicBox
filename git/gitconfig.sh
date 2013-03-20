114df08129fc9badebfade0b23e499e3c07da6c8
Leo Jiang <leo.jiang.dev@gmail.com>
2013-03-20 21:56:59 +0800

    set the push.default to simple as recommended.

--- git/gitconfig.sh
+++ git/gitconfig.sh
@@ -4,6 +4,7 @@ git config --global user.email leo.jiang.dev@gmail.com
 git config --global core.editor vim
 git config --global core.excludesfile "~/.gitignore_global"
 git config --global core.autocrlf input
+git config --global push.default simple
 git config --global alias.ci commit
 git config --global alias.st status
 cp _gitignore_global ~/.gitignore_global
