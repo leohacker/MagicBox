a8a0a2fa3e6571e359ba54cf4288e8472b729311
Leo Jiang <leo.jiang.dev@gmail.com>
2013-04-16 16:09:53 +0800

    update the README for moinmoin upgrade script.

--- moin/moin-upgrade.sh
+++ moin/moin-upgrade.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
+# Moin 1.9
+#
 # Usage:
-# unpack the latest moin tar package in ~/moin, then run upgrade script to 
+# unpack the latest moin tar package in ~/moin, then run upgrade script to
 #  1. customize moin installation.
 #  2. link the page from Dropbox folder, ~/Dropbox.
 #
 # ./moin-upgrade.sh ~/moin
 
+function usage {
+    echo 'moin-upgrade.sh $moin-install-dir'
+}
+
+if [ $# == 0 ]; then
+    usage
+    exit 0
+fi
+
+if [ $# != 1 ]; then
+    echo "Please specify the installation directory of MoinMoin."
+    usage
+    exit 0
+fi
+
 MOIN_INSTDIR=$1
+
 cp wikiconfig.py ${MOIN_INSTDIR}
 cp gugiel.py ${MOIN_INSTDIR}/MoinMoin/theme
 cp -a gugiel ${MOIN_INSTDIR}/MoinMoin/web/static/htdocs
