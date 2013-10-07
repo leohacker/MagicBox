5f32ec9ca7d38731f9ca306de1534b95806576bc
Leo Jiang <leo.jiang.dev@gmail.com>
2013-10-07 18:18:49 +0800

    add new aliases for git config.

--- git/gitconfig.sh
+++ git/gitconfig.sh
@@ -5,8 +5,11 @@ git config --global core.editor vim
 git config --global core.excludesfile "~/.gitignore_global"
 git config --global core.autocrlf input
 git config --global push.default simple
+git config --global alias.br branch
 git config --global alias.ci commit
+git config --global alias.co checkout
 git config --global alias.st status
+git config --global alias.unstage 'reset HEAD --'
 git config --global credential.helper osxkeychain
 git config --global color.ui true
 cp _gitignore_global ~/.gitignore_global
