32d76585b32b412d371a5dd34a03a54e68a93e13
Leo Jiang <leo.jiang.dev@gmail.com>
2013-11-24 16:46:07 +0800

    Set the heap size for tomcat.

--- bash/bash.d/java.bash
+++ bash/bash.d/java.bash
@@ -2,4 +2,6 @@
 # JAVA TOMCAT
 export JAVA_HOME=$(/usr/libexec/java_home)
 export CATALINA_HOME=~/Applications/apache-tomcat
+export CATALINA_OPTS="-Xms1024M -Xmx1024M"
+
 export ANT_HOME=/usr/share/ant
