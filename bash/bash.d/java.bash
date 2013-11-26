0f74ed831aefb6e1033003c314245df3a81becbc
Leo Jiang <leo.jiang.dev@gmail.com>
2013-11-26 23:25:42 +0800

    rename the directory for tomcat.

--- bash/bash.d/java.bash
+++ bash/bash.d/java.bash
@@ -1,7 +1,7 @@
 # MacOSX
 # JAVA TOMCAT
 export JAVA_HOME=$(/usr/libexec/java_home)
-export CATALINA_HOME=~/Applications/apache-tomcat
+export CATALINA_HOME=~/Applications/tomcat
 export CATALINA_OPTS="-Xms1024M -Xmx1024M"
 
 export ANT_HOME=/usr/share/ant
