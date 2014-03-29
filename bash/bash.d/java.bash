04a1ca25cfb52d5bab3a94ae97dc031cee11ff54
Leo Jiang <leo.jiang.dev@gmail.com>
2014-03-29 21:22:11 +0800

    update the java and ruby bashrc.

--- bash/bash.d/java.bash
+++ bash/bash.d/java.bash
@@ -2,6 +2,5 @@
 # JAVA TOMCAT
 export JAVA_HOME=$(/usr/libexec/java_home)
 export CATALINA_HOME=~/Applications/tomcat
-export CATALINA_OPTS="-Xms1024M -Xmx1024M"
 
 export ANT_HOME=/usr/share/ant
--- bash/bash.d/ruby.bash
+++ bash/bash.d/ruby.bash
@@ -3,5 +3,8 @@
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 # source the completion for rvm.
 [[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
+## source the prompt function and apply it.
+#[[ -s "$HOME/.rvm/contrib/ps1_functions" ]] && . "$HOME/.rvm/contrib/ps1_functions"
+#ps1_set
 # Add RVM to PATH for scripting
 PATH=$PATH:$HOME/.rvm/bin
